d1f926a575352610bdf34bdcc2055e457d0ec5a9
Ezequiel <devezek@gmail.com>
2018-04-13 12:21:03 +0200

    Changed to read from arguments to env variables for watchdog register.

--- flags.go
+++ flags.go
@@ -2,28 +2,30 @@ package orion
 
 import (
 	"flag"
+	"os"
 	"strconv"
 )
 
 // ParseFlags will parse the flags if they are not parsed yet
 // If they are already parsed the func will lookup for the "--verbose" and "--watchdog"
 func parseFlags() {
+	registerToWatchdogByDefault = new(bool)
+	wd := os.Getenv("WATCHDOG")
+	if wd == "true" || wd == "1" {
+		*registerToWatchdogByDefault = true
+	} else {
+		*registerToWatchdogByDefault = false
+	}
+
 	if flag.Parsed() {
 		v := flag.Lookup("verbose")
 		if v != nil {
 			b, _ := strconv.ParseBool(v.Value.String())
 			verbose = &b
 		}
-		w := flag.Lookup("watchdog")
-		if w != nil {
-			b, _ := strconv.ParseBool(w.Value.String())
-			registerToWatchdogByDefault = &b
-		}
 	} else {
-		bw := flag.Bool("watchdog", false, "Register to watchdog at init and periodically")
 		bv := flag.Bool("verbose", false, "Enable verbose (console) logging")
 		flag.Parse()
-		registerToWatchdogByDefault = bw
 		verbose = bv
 	}
 }
