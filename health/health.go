ab6da0fbd7c73f0efe2e26e16117f5fc9c520ddc
Ezequiel Santamaria <devezek@gmail.com>
2020-02-12 13:02:31 +0100

    fixed: a race condition over the loop of health checks.

--- health/health.go
+++ health/health.go
@@ -1,6 +1,7 @@
 package health
 
 import (
+	"sync"
 	"time"
 )
 
@@ -12,21 +13,28 @@ const (
 // LoopOverHealthChecks loops FOREVER over the HealthChecks that are given.
 func LoopOverHealthChecks(dependencies []Dependency, close chan struct{}) {
 	run := true
+	runLock := sync.Mutex{}
 	delay := make(chan struct{}, 1)
 	delay <- struct{}{}
 
 	// This go function produces an event to the channel for the loop.
 	go func() {
+		runLock.Lock()
 		for run {
+			runLock.Unlock()
 			time.Sleep(defaultHealthCheckLoopPeriod)
 			delay <- struct{}{}
+			runLock.Lock()
 		}
+		runLock.Unlock()
 	}()
 
 	for run {
 		select {
 		case <-close: // We want to end up the loop
+			runLock.Lock()
 			run = false
+			runLock.Unlock()
 		case <-delay: // Every loop period we run the health checks.
 			ResetSummaryOfHealthChecks()
 			for _, dep := range dependencies {
